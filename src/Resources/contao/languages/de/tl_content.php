b4c7c08034ee5ae3eaef54c24bc46098c7e3e3f9
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-03-16 01:24:52 +0100

    Create tl_content.php

---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,26 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['vtxm'] = 'VTXM';
+$GLOBALS['TL_LANG']['CTE']['layout_preset'] = ['Layout Preset', 'Layout-Wrapper mit zwei frei befüllbaren Slots.'];
+
+$GLOBALS['TL_LANG']['tl_content']['layout_legend'] = 'Layout';
+$GLOBALS['TL_LANG']['tl_content']['slot_a_legend'] = 'Slot A';
+$GLOBALS['TL_LANG']['tl_content']['slot_b_legend'] = 'Slot B';
+
+$GLOBALS['TL_LANG']['tl_content']['layoutPreset'] = ['Preset', 'Layout-Preset wählen.'];
+$GLOBALS['TL_LANG']['tl_content']['layoutMode'] = ['Anordnung', 'Anordnung der beiden Bereiche.'];
+$GLOBALS['TL_LANG']['tl_content']['layoutAlign'] = ['Vertikale Ausrichtung', 'Vertikale Ausrichtung der Bereiche.'];
+$GLOBALS['TL_LANG']['tl_content']['layoutDivider'] = ['Divider anzeigen', 'Zeigt einen visuellen Trenner zwischen den Bereichen.'];
+$GLOBALS['TL_LANG']['tl_content']['layoutStackMobile'] = ['Auf Mobile stapeln', 'Die Bereiche auf kleinen Viewports untereinander anzeigen.'];
+
+$GLOBALS['TL_LANG']['tl_content']['slotAType'] = ['Slot A Typ', 'Quelle für Slot A.'];
+$GLOBALS['TL_LANG']['tl_content']['slotAPage'] = ['Slot A Seite', 'Seite auswählen, aus der der Artikel geladen wird.'];
+$GLOBALS['TL_LANG']['tl_content']['slotAArticle'] = ['Slot A Artikel', 'Artikel auswählen.'];
+$GLOBALS['TL_LANG']['tl_content']['slotAModule'] = ['Slot A Modul', 'Frontend-Modul auswählen.'];
+$GLOBALS['TL_LANG']['tl_content']['slotAHtml'] = ['Slot A HTML', 'Freier HTML-/RTE-Inhalt für Slot A.'];
+
+$GLOBALS['TL_LANG']['tl_content']['slotBType'] = ['Slot B Typ', 'Quelle für Slot B.'];
+$GLOBALS['TL_LANG']['tl_content']['slotBPage'] = ['Slot B Seite', 'Seite auswählen, aus der der Artikel geladen wird.'];
+$GLOBALS['TL_LANG']['tl_content']['slotBArticle'] = ['Slot B Artikel', 'Artikel auswählen.'];
+$GLOBALS['TL_LANG']['tl_content']['slotBModule'] = ['Slot B Modul', 'Frontend-Modul auswählen.'];
+$GLOBALS['TL_LANG']['tl_content']['slotBHtml'] = ['Slot B HTML', 'Freier HTML-/RTE-Inhalt für Slot B.'];
